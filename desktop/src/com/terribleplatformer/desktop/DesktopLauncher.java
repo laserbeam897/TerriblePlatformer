5b84dd9548ed671137961d2d946a389762416898
Michael <michaelbarry564@gmail.com>
2018-02-03 13:51:14 +0000

    change the window size to 1280 by 720

--- desktop/src/com/terribleplatformer/desktop/DesktopLauncher.java
+++ desktop/src/com/terribleplatformer/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.terribleplatformer.TerribleGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new TerribleGame(), config);
 	}
 }
